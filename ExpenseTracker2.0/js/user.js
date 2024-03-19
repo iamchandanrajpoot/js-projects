50fac4004418efab4b8b450d9a50ec5164ee5f96
iamchandanrajpoot <iamchandanrajpoot@gmail.com>
2024-03-19 09:09:25 +0530

    transaction fails update the ORDER status from PENDING to FAILED in the database.

--- ExpenseTracker2.0/js/user.js
+++ ExpenseTracker2.0/js/user.js
@@ -1,54 +1,54 @@
 function handleRegister(e) {
-  e.preventDefault();
-  const userData = {
-    name: e.target.name.value,
-    email: e.target.email.value,
-    password: e.target.password.value,
-  };
-  fetch("http://localhost:4000/user/register", {
-    method: "post",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userData),
-  })
-    .then((response) => {
-      document.getElementById("name").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("password").value = "";
-      console.log(response);
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      if (data) document.getElementById("message").innerHTML = data.message;
-    })
-    .catch((err) => console.log(err));
-}
-
-async function handleLogin(e) {
-  try {
     e.preventDefault();
     const userData = {
+      name: e.target.name.value,
       email: e.target.email.value,
       password: e.target.password.value,
     };
-    const response = await fetch("http://localhost:4000/user/login", {
+    fetch("http://localhost:4000/user/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(userData),
-    });
-    console.log(response);
-    if (response.status === 200) {
-      const result = await response.json();
-      console.log(result);
-      localStorage.setItem("authToken", result.token);
-      window.location.href = "expense.html";
-    } else {
-      const result = await response.json();
-      // console.log(result);
-      document.getElementById("message").innerHTML = result.message;
-    }
-  } catch (error) {
-    console.log(error);
+    })
+      .then((response) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (data) document.getElementById("message").innerHTML = data.message;
+      })
+      .catch((err) => console.log(err));
   }
-
-}
+  
+  async function handleLogin(e) {
+    try {
+      e.preventDefault();
+      const userData = {
+        email: e.target.email.value,
+        password: e.target.password.value,
+      };
+      const response = await fetch("http://localhost:4000/user/login", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+      });
+      console.log(response);
+      if (response.status === 200) {
+        const result = await response.json();
+        console.log(result);
+        localStorage.setItem("authToken", result.token);
+        window.location.href = "expense.html";
+      } else {
+        const result = await response.json();
+        // console.log(result);
+        document.getElementById("message").innerHTML = result.message;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  
+  }
